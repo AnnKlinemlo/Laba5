b5bd19ad87b4f4d828d10c10ebdf785e04fba1f3
Anna Klimenko <115544898+AnnKlinemlo@users.noreply.github.com>
2022-12-15 10:29:50 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,91 +1,82 @@
-import io
-from tkinter import Image
 
-import keras
-from PIL import ImageQt
-from keras.models import load_model
-import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 
-import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-import io
-from PIL import Image
-from tkinter.messagebox import showinfo
+train_dir=r"C:\Users\Аня\Desktop\Work\ISIT\mymodel\train"
+test_dir=r"C:\Users\Аня\Desktop\Work\ISIT\mymodel\test"
+val_dir=r"C:\Users\Аня\Desktop\Work\ISIT\mymodel\val"
 
-model = keras.models.load_model(r'C:\Users\Аня\Desktop\Work\ISIT\mymodel')
-name = np.array(['Круг', 'Квадрат', 'Звезда', 'Треугольник'])
+img_width, img_height = 200, 200
+input_shape = (img_width, img_height, 3)
+
+batch_size = 20
+
+epochs = 20
+# Количество изображений для обучения
+nb_train_samples = 2706*4
+# Количество изображений для проверки
+nb_validation_samples = 460*4
+# Количество изображений для тестирования
+nb_test_samples = 561*4
+
+datagen = ImageDataGenerator(rescale=1./255, validation_split=0.4)
+
+train_generator = datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    subset="training",
+    class_mode='categorical')
+
+val_generator = datagen.flow_from_directory(
+    val_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    subset="validation",
+    class_mode='categorical')
+
+test_generator = datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
 
 
-def predict_digit(img):
-    buffer = QBuffer()
-    buffer.open(QBuffer.ReadWrite)
-    img.save(buffer, "PNG")
-    img = Image.open(io.BytesIO(buffer.data()))
-    img = img.convert('RGB')
-    img = img.resize((200, 200))
-    img = np.array(img)
-    x = np.asarray([np.array(img)])
-    predict_result = model.predict(x, verbose=2)
-    result_ind = np.argmax(predict_result)
-    return name[result_ind]
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-class App(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Лабораторная5")
-        self.setGeometry(400, 400, 400, 400)
-        self.image = QImage(self.size(), QImage.Format_RGB32)
-        self.image.fill(Qt.white)
-        self.label = QLabel("", self)
-        self.label.setGeometry(125, 100, 100, 100)
-        self.drawing = False
-        self.brushSize = 40
-        self.brushColor = Qt.black
-        self.lastPoint = QPoint()
-        mainMenu = self.menuBar()
-        recognizeAction = QAction("Ответ", self)
-        mainMenu.addAction(recognizeAction)
-        recognizeAction.triggered.connect(self.recognize)
-        clearAction = QAction("Очистить", self)
-        mainMenu.addAction(clearAction)
-        clearAction.triggered.connect(self.clear)
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            self.drawing = True
-            self.lastPoint = event.pos()
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    def mouseMoveEvent(self, event):
-        if (event.buttons() & Qt.LeftButton) & self.drawing:
-            painter = QPainter(self.image)
-            painter.setPen(QPen(self.brushColor, self.brushSize, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
-            painter.drawLine(self.lastPoint, event.pos())
-            self.lastPoint = event.pos()
-            self.update()
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4, activation='softmax'))
 
-    def mouseReleaseEvent(self, event):
-        if event.button == Qt.LeftButton:
-            self.drawing = False
+model.summary()
 
-    def paintEvent(self, event):
-        canvasPainter = QPainter(self)
-        canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
-    def recognize(self):
-        o = predict_digit(self.image)
-        showinfo(title="Ответ", message="Вы нарисовали: " + o)
 
-    def clear(self):
-        self.image.fill(Qt.white)
-        self.update()
+x=train_generator
+s_p_e=nb_train_samples // batch_size
+v_s=nb_validation_samples // batch_size
+model.fit(x,
+    steps_per_epoch=s_p_e,
+    epochs=epochs,
+    validation_data=val_generator,
+    validation_steps=v_s)
 
-def main():
-    app = QApplication(sys.argv)
-    window = App()
-    window.show()
-    app.exec()
+step=nb_test_samples // batch_size
 
-if __name__ == '__main__':
-    main()
+scores = model.evaluate_generator(test_generator, step)
+
+print("Аккуратность на тестовых данных: %.2f%%" % (scores[1]*100))
+model.save(r'C:\Users\Аня\Desktop\Work\ISIT\mymodel')
